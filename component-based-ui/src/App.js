d506eaa105dae47f8c5ba8630742c70d680c98c5
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-09-22 15:58:11 +0300

    test

--- component-based-ui/src/App.js
+++ component-based-ui/src/App.js
@@ -5,34 +5,91 @@ import Header from './component/Header';
 import FormComponent from './component/FormComponent';
 import Result from './component/Result';
 import React, { useState } from 'react';
+import axios from 'axios';
+import Loading from './component/loading';
 
 
 function App() {
-  
+
 
   const [data, setData] = useState(
-    []
+    {headers:'headers', data:"Results"}
   );
-  
 
-  const submitFun = (e) =>{
+  const [method, setMethod] = useState(
+    'get'
+  );
+
+  const [loading, setLoading] = useState(
+    false
+  );
+
+  const submitFun = async (e) => {
     e.preventDefault();
 
     let myData = e.target.email.value
+
+    let myButton = e.target.myButton.textContent
+
+    // console.log(myButton)
+    let resultData
+
+    setLoading(true)
+
+    try{
+      if(myButton === 'get'){
+        console.log('get')
+        resultData = await axios.get(myData)
+      }else if(myButton === 'post'){
+        console.log('post')
+        resultData = await axios.post(myData)
+  
+      }else if(myButton === 'delete'){
+        console.log('delete')
+        resultData = await axios.delete(myData)
+  
+      }else if(myButton === 'put'){
+        console.log('put')
+        resultData = await axios.put(myData)
+  
+      }
+  
+      setMethod(myButton)
+      setLoading(false)
+      let lastResult = resultData
+      setData(lastResult)
+  
+      console.log(resultData);
+  
+  
+    }catch(err){
+      setMethod(myButton)
+  
+      setLoading(false)
+      let newObj = {
+        headers: `Faild to ${myButton} this link ${myData}`,
+        data: err
+      }
+      let lastResult = newObj
+      setData(lastResult)
+  
+      console.log(err);
+  
+    }
     
-
-
-    setData(`get :  ${myData}`)
-    
-
   }
 
   return (
     <div className="App">
       <Header myHeader={'RESTy'} />
       <FormComponent submitFun={submitFun} />
-      <Result data={data} />
-      
+      {
+        loading &&
+         <Loading />
+      }
+     
+      <Result method={method} data={data} />
+
       <Footer myFooter={'© 2018 Code Fellows'} />
     </div>
   );
---
+++ component-based-ui/src/component/loading.js
@@ -0,0 +1,15 @@
+import "../styles/Loading.scss"
+
+function Loading (){
+
+    return (
+      <>
+      {/* <p>Loading......</p> */}
+      <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"  />
+      </>
+      
+    );
+
+}
+
+export default Loading;
