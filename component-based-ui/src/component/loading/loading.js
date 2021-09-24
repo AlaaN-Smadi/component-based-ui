446a6b2596ef855874cf87ae12aa9d8b8768b6f8
AlaaN-Smadi <82310892+AlaaN-Smadi@users.noreply.github.com>
2021-09-24 15:06:47 +0300

    Update loading.js

--- component-based-ui/src/component/loading/loading.js
+++ component-based-ui/src/component/loading/loading.js
@@ -5,7 +5,7 @@ function Loading (){
     return (
       <>
       {/* <p>Loading......</p> */}
-      <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"  />
+      <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif" alt="Loading ..."  />
       </>
       
     );
